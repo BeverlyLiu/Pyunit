3d9f30ffaa12a6b82735f747b0432a11356e1760
Beverly Liu <bevliu@gmail.com>
2018-09-30 12:37:25 -0700

    Amazon test selenium java

---
+++ AmazonTest.java
@@ -0,0 +1,38 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonTest {
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("http://www.amazon.com/");
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void testAmazonSearchResult() {
+		WebElement input = driver.findElement(By.id("twotabsearchtextbox"));
+		input.sendKeys("selenium webdriver");
+		WebElement searchIcon =driver.findElement(By.cssSelector("#nav-search > form > div.nav-right > div > input"));
+		
+		searchIcon.click();
+		WebElement status = driver.findElement(By.id("s-result-count"));
+		String text = "1-16 of 139 results for \"selenium webdriver\"";
+		assertEquals(text, status.getText());
+	}
+}
